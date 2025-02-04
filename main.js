306f737e7e507d880a577be914bf544a7b23a821
Mazen <ahedmazenm@outlook.com>
2025-02-04 17:09:50 +0200

    added signup and login in modals using bootstrap also added validation for user input using jq

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { failurMessage, successMassage } from "./messages.js";
+
 $(document).ready(function() {
     $('.nav a').click(function(e) {
         e.preventDefault();
@@ -24,6 +26,59 @@ $(document).ready(function() {
     $('#scrollTop').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 800);
     });
-
-
 });
+
+class Validator {
+    static nameValidation(name) {
+        const nameRegex = /^[a-zA-Z\s]+$/;
+        if (nameRegex.test(name)) {
+            return 1;
+        } else {
+            return "Not valid Validator name";
+        }
+    }
+
+    static passwordValidation(password) {
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+        if (passwordRegex.test(password)) {
+            return 1;
+        } else {
+            return `Password not valid:
+                    1-At least 6 characters long
+                    2-Contains at least one letter
+                    3-Contains at least one digit
+            `;
+        }
+    }
+
+    static emailValidation(email) {
+        const emailRegex = /^[a-zA-Z][^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    static phoneValidation(phone) {
+        const phoneRegex = /^(?:\+20|0)?1[0125]\d{8}$/;
+        if (phoneRegex.test(phone)) {
+            return 1;
+        } else {
+            return "Not valid phone number";
+        }
+    }
+
+    static addressValidation(address) {
+        const addressRegex = /^[a-zA-Z0-9\s,]+$/;
+        if (addressRegex.test(address)) {
+            return 1;
+        } else {
+            return "Not valid address";
+        }
+    }
+
+    static ageValidation(age) {
+        if (age >= 15 && age <= 120) {
+            return 1;
+        } else {
+            return "You need to be older than 15 years old to sign up";
+        }
+    }
+}
